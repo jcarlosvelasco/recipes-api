8c48374f0e5bbd27c443f334384de7777fcbfefa
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2023-12-18 19:49:10 +0100

    createRecipe and deleteRecipe functions built

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,13 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
 import play.mvc.*;
+import views.RecipeResource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -14,8 +21,56 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+
     public Result index() {
         return ok(views.html.index.render());
     }
 
+
+    List<RecipeResource> recipeResList = new ArrayList<>();
+
+    public Result listRecipes() {
+        System.out.println("Listar recetas");
+
+        JsonNode json = Json.toJson(recipeResList);
+
+        return ok(json);
+    }
+
+    public Result getRecipe(String recipeID) {
+        return ok();
+    }
+
+    public Result updateRecipe(String recipeID) {
+        return ok();
+    }
+
+    public Result createRecipe() {
+        RecipeResource recipeRes = new RecipeResource();
+        recipeRes.setName("Pollo frito");
+
+        recipeResList.add(recipeRes);
+
+        JsonNode json = Json.toJson(recipeRes);
+
+        return created(json);
+    }
+
+    public Result deleteRecipe(String recipeID) {
+        int index = -1;
+
+        for (int i = 0; i < recipeResList.size(); i++) {
+            if (Objects.equals(recipeResList.get(i).getId(), recipeID)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            return notFound();
+        }
+
+        recipeResList.remove(index);
+        return ok();
+    }
 }
---
+++ app/views/RecipeResource.java
@@ -0,0 +1,28 @@
+package views;
+
+import java.util.UUID;
+
+public class RecipeResource {
+    String id;
+    String name;
+
+    public RecipeResource() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
