9298b15cc53a571848fbe21981b25407c381d491
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2023-12-26 11:10:47 +0100

    Fixed model imports

--- app/models/RecipeModel.java
+++ app/models/RecipeModel.java
@@ -1,7 +1,67 @@
 package models;
 
 import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+import io.ebean.annotation.WhenModified;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import java.sql.Timestamp;
 
+@Entity
 public class RecipeModel extends Model  {
 
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Version
+    private Long version;
+
+    @WhenCreated
+    private Timestamp whenCreated;
+
+    @WhenModified
+    private Timestamp whenModified;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Timestamp getWhenCreated() {
+        return whenCreated;
+    }
+
+    public void setWhenCreated(Timestamp whenCreated) {
+        this.whenCreated = whenCreated;
+    }
+
+    public Timestamp getWhenModified() {
+        return whenModified;
+    }
+
+    public void setWhenModified(Timestamp whenModified) {
+        this.whenModified = whenModified;
+    }
 }
